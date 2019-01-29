12301acfc40eaf48b3cd61fcbf6a920c9c21917e
Urban Borštnik <urban.borstnik@id.ethz.ch>
2019-01-29 09:29:37 +0100

    Use built-in test repository instead for unit tests. Improves unit tests.
    Signed-off-by: Urban Borštnik <urban.borstnik@id.ethz.ch>

--- test_dependency_versioning.py
+++ test_dependency_versioning.py
@@ -7,12 +7,23 @@ import unittest
 
 import tempfile
 import subprocess
+import os
 
 import yaml
 
 import dependency_versioning as dv
 
 class TestDependencyVersioning(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.tempdir_name = self.tempdir.name
+        subprocess.Popen(
+            "OD=$PWD&&cd {dir:s}&&tar xzf $OD/test_repo.tgz".format(dir=self.tempdir_name),
+            shell="True", universal_newlines=True).communicate()
+
+    def tearDown(self):
+        self.tempdir.cleanup()
+
     def test_read_vif(self):
         "Test reading vifs."
         vif = {
@@ -43,26 +54,59 @@ class TestDependencyVersioning(unittest.TestCase):
 
     def test_git_clone(self):
         try:
-            (stdout, stderr) = subprocess.Popen("rm -fr user_service_manager", shell=True, universal_newlines=True).communicate()
+            (stdout, stderr) = subprocess.Popen("rm -fr test_repo", shell=True, universal_newlines=True).communicate()
         except:
             pass
-        git_dep = dv.GITDependency(name="user_service_manager", repository="git@hpc-git.ethz.ch:user_service_manager", branch="master")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master")
         git_dep.update()
 
     def test_git_update(self):
-        git_dep = dv.GITDependency(name="user_service_manager", repository="git@hpc-git.ethz.ch:user_service_manager", branch="master")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master")
         git_dep.update()
-        print("TEST: HEAD IS AT", git_dep.get_present_version())
-        (stdout, stderr) = subprocess.Popen("cd user_service_manager && git reset --hard HEAD^; git status", shell=True, universal_newlines=True).communicate()
-        print("TEST: HEAD IS AT", git_dep.get_present_version())
+        self.assertEqual(git_dep.get_present_version(), "4cc3b223d2997c8bbf84bfeae420b0d1e4dad732")
+        (stdout, stderr) = subprocess.Popen("cd test_repo && git reset --hard HEAD^", shell=True, universal_newlines=True).communicate()
+        self.assertEqual(git_dep.get_present_version(), "395c738b6aa8b7074d7d2e533c95276cc0990876")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master")
         git_dep.update()
-        print("TEST: HEAD IS AT", git_dep.get_present_version())
+        self.assertEqual(git_dep.get_present_version(), "4cc3b223d2997c8bbf84bfeae420b0d1e4dad732")
+
+    def test_git_change_branch(self):
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master")
+        git_dep.update()
+        self.assertEqual(git_dep.get_present_version(), "4cc3b223d2997c8bbf84bfeae420b0d1e4dad732")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="dev")
+        git_dep.update()
+        self.assertEqual(git_dep.get_present_version(), "238aacacf8d4c0a73f3395912c5114763fa195ab")
 
     def test_git_set_version(self):
-        git_dep = dv.GITDependency(name="user_service_manager", repository="git@hpc-git.ethz.ch:user_service_manager", branch="master", version="a7df4b7")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master")
         git_dep.update()
-        print("TEST: HEAD IS AT", git_dep.get_present_version())
-        
+        self.assertEqual(git_dep.get_present_version(), "4cc3b223d2997c8bbf84bfeae420b0d1e4dad732")
+        git_dep = dv.GITDependency(
+            name="test_repo",
+            repository=os.path.join(self.tempdir_name, "test_repo"),
+            branch="master",
+            version="395c738b6aa8b7074d7d2e533c95276cc0990876")
+        git_dep.update()
+        self.assertEqual(git_dep.get_present_version(), "395c738b6aa8b7074d7d2e533c95276cc0990876")
         
 if __name__ == "__main__":
     main()
